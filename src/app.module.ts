3aa805fc288c2e910d544101430023534ad3be29
Vladimir Traigel <gtc.traigel@gmail.com>
2022-12-06 16:00:24 +0100

    add handling of the .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { CoinsModule } from './coins/coins.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronModule } from './cron/cron.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     MongooseModule.forRoot('mongodb://mongodb:27017/coinlense'),
     ScheduleModule.forRoot(),
     CoinsModule,
